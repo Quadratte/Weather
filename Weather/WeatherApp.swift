7adb99b527bbf62d57da861fd0afd34ec94439bb
Sergey Philippov <quadratte@gmail.com>
2025-05-15 12:57:47 +0700

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Quadratte on 15.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
